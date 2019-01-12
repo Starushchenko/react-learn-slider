79d63bf8e08b16e7a0bc2e5ab23326e6b3811f34
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2019-01-12 19:01:16 +0300

    Урок 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+const bgUrl = process.env.PUBLIC_URL + '/img/background.jpg';
+
+const HeaderWrapper = styled.header`
+	width: 100%;
+	min-height: 49px;
+	background-color: #242424;
+	opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+	min-height: 89px;
+	padding-top: 20px;
+`
+
+const MainWrapper = styled.div`
+	min-height: 600px;
+	padding-top: 130px;
+	background: url(${bgUrl}) no-repeat;
+	background-size: cover;
+`
+
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello World!</h1>
-    );
-  }
+	render() {
+		return (
+				<div className="App">
+					<HeaderWrapper>
+						<Grid>
+							<Header/>
+						</Grid>
+					</HeaderWrapper>
+					<MenuWrapper>
+						<Grid>
+							<Menu/>
+						</Grid>
+					</MenuWrapper>
+					<MainWrapper>
+						<Grid>
+							<Main/>
+						</Grid>
+					</MainWrapper>
+				</div>
+		);
+	}
 }
 
 export default App;

9850149e27c6aca26f5b625e02bc9765d5dfe4fe
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2019-01-16 14:31:29 +0300

    Билд

---
+++ build/precache-manifest.200646c542e5f474ac03b143962e4377.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "97cd79e808f70e979f9c8ef95a214494",
+    "url": "/static/media/slide-4.97cd79e8.jpg"
+  },
+  {
+    "revision": "2ff1bedcba4a97a5accc60b4f4a4dbcf",
+    "url": "/static/media/slide-3.2ff1bedc.jpg"
+  },
+  {
+    "revision": "b8eae6f3d43fce4a5fdfd59a998fd3ec",
+    "url": "/static/media/slide-2.b8eae6f3.jpg"
+  },
+  {
+    "revision": "e5976c8e4ff66583f72006bb23dca7a6",
+    "url": "/static/media/slide-1.e5976c8e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d109cd35290334ed5e9",
+    "url": "/static/js/main.7d109cd3.chunk.js"
+  },
+  {
+    "revision": "fcb6342d07f150b1c9df",
+    "url": "/static/js/1.fcb6342d.chunk.js"
+  },
+  {
+    "revision": "7d109cd35290334ed5e9",
+    "url": "/static/css/main.f7182144.chunk.css"
+  },
+  {
+    "revision": "ec650013ff88cad7d13053c7cb9c0420",
+    "url": "/index.html"
+  }
+];
